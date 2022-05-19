df87aabfd8d09b6ba1e8b7b2721b2fc5f965979d
Wender Gonçalves <wendergs2@gmail.com>
2022-05-18 21:51:58 -0300

    version 0.03

--- htdocs/js/mes.js
+++ htdocs/js/mes.js
@@ -3,6 +3,8 @@
 let data = new Date()
 let monthBruto = (data.getMonth()+1)
 
+let month = null
+
 switch(monthBruto){
     case 1:
         month = '01'
@@ -32,8 +34,6 @@ switch(monthBruto){
         month = '09'
         break
 }
-
-console.log(month)
 
 $(document).ready(() => {
     
@@ -104,19 +104,171 @@ function getDataByMonth(m = month){
 }
 
 
-const selected = document.querySelector('.selected')
-const optionsContainer = document.querySelector('.options-container')
-const optionList = document.querySelectorAll('.option')
 
-optionList.forEach(o => {
-    o.addEventListener('click', () => {
-        selected.innerHTML = o.querySelector('label').innerHTML
-        optionsContainer.classList.remove('active-select')
-        getDataByMonth(o.querySelector('input').value)
-        //console.log()
+let calendar = document.querySelector('#show-calendar')
+let calendarBox = document.querySelector('#calendar')
+let months = document.querySelectorAll('.m')
+
+let left = document.querySelector('#left')
+let right = document.querySelector('#right')
+let year = document.querySelector('#year')
+
+let date = new Date()
+let month2 = (date.getMonth() + 1)
+let fullYear = date.getFullYear()
+let monthDefault = (date.getMonth() + 1)
+
+switch(monthDefault){
+    case 1:
+        month2 = '01'
+        break
+    case 2:
+        month2 = '02'
+        break
+    case 3:
+        month2 = '03'
+        break
+    case 4:
+        month2 = '04'
+        break
+    case 5:
+        month2 = '05'
+        break
+    case 6:
+        month2 = '06'
+        break
+    case 7:
+        month2 = '07'
+        break
+    case 8:
+        month2 = '08'
+        break
+    case 9:
+        month2 = '09'
+        break
+}
+
+
+switch(month){
+    case '01':
+        mes.innerHTML = 'Jan'
+        break
+    case '02':
+        mes.innerHTML = 'Fev'
+        break
+    case '03':
+        mes.innerHTML = 'Mar'
+        break
+    case '04':
+        mes.innerHTML = 'Abr'
+        break
+    case '05':
+        mes.innerHTML = 'Mai'
+        break
+    case '06':
+        mes.innerHTML = 'Jun'
+        break
+    case '07':
+        mes.innerHTML = 'Jul'
+        break
+    case '08':
+        mes.innerHTML = 'Ago'
+        break
+    case '09':
+        mes.innerHTML = 'Set'
+        break
+    case 10:
+        mes.innerHTML = 'Out'
+        break
+    case 11:
+        mes.innerHTML = 'Nov'
+        break
+    case 12:
+        mes.innerHTML = 'Dez'
+        break
+}
+
+year.innerHTML = fullYear
+
+left.addEventListener('click', () => {
+    year.innerHTML -= 1
+})
+
+right.addEventListener('click', () => {
+    year.innerHTML = parseInt(year.innerHTML)+1
+})
+
+months.forEach(m => {
+    
+
+    m.addEventListener('click', () => {
+        mes.innerHTML = m.innerHTML
+        calendarBox.classList.toggle('d-none')
+        getDataForDash(m.innerHTML)
+        
     })
 })
 
-selected.addEventListener('click', () => {
-    optionsContainer.classList.toggle('active-select')
+calendar.addEventListener('click', () => {
+    calendarBox.classList.toggle('d-none')
 })
+
+
+function getDataForDash(month = monthDefault){
+    let newMonth = null
+    const xhttp = new XMLHttpRequest()
+    switch(month){
+        case 'jan':
+                newMonth = '01'
+            break
+        case 'fev':
+            newMonth = '02'
+            break
+        case 'mar':
+            newMonth = '03'
+            break
+        case 'abr':
+            newMonth = '04'
+            break
+        case 'mai':
+            newMonth = '05'
+            break
+        case 'jun':
+            newMonth = '06'
+            break
+        case 'jul':
+            newMonth = '07'
+            break
+        case 'ago':
+            newMonth = '08'
+            break
+        case 'set':
+            newMonth = '09'
+            break
+        case 'out':
+            newMonth = '10'
+            break
+        case 'nov':
+            newMonth = '11'
+            break
+        case 'dez':
+            newMonth = '12'
+            break
+    }
+
+    xhttp.onreadystatechange = () => {
+        if(xhttp.status === 200 && xhttp.readyState === 4){
+            let dados = JSON.parse(xhttp.responseText)
+
+            document.querySelector('#saldo').innerHTML = `R$ ${(dados.totalRent - dados.totalExpense).toFixed(2)}`
+            document.querySelector('#rent').innerHTML = `R$ ${dados.totalRent ? dados.totalRent.toFixed(2) : (0).toFixed(2)}`
+            document.querySelector('#expense').innerHTML = `R$ ${dados.totalExpense ? dados.totalExpense.toFixed(2) : (0).toFixed(2)}`
+
+
+
+        }
+    }
+    xhttp.open('GET', `/getDataByMonth?month=${newMonth}`)
+    xhttp.send()
+
+}   
